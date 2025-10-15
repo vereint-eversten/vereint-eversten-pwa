10cbcff6b064d654ae5f2dea7a2c075d33bb7512
vereint-eversten <fab_f1ve@web.de>
2025-10-15 16:51:46 +0200

    Update kalender.js

--- js/kalender.js
+++ js/kalender.js
@@ -86,7 +86,7 @@ async function renderCalendar(date = new Date()) {
   for (let i = 0; i < startDay; i++) html += <div class="cell empty"></div>;
 
   for (let d = 1; d <= daysInMonth; d++) {
-    const thisDate = ${year}-${String(month + 1).padStart(2, "0")}-${String(d).padStart(2, "0")};
+    const thisDate = `${year}-${String(month + 1).padStart(2, "0")}-${String(d).padStart(2, "0")}`;
     const dayEvents = events.filter(ev => ev.date === thisDate);
 
     let eventsHTML = "";
