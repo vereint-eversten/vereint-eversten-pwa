14c50f2a64f5c52971f47bd6df31fe6ad8a6e0e6
vereint-eversten <fab_f1ve@web.de>
2025-10-16 17:32:46 +0200

    Update kalender.js

--- assets/js/kalender.js
+++ assets/js/kalender.js
@@ -206,7 +206,7 @@
         values.forEach(v => {
           const opt = document.createElement("option");
           opt.value = v;
-          if ($sel.id === "gradeFilter") opt.textContent = ${v}. Klasse;
+          if ($sel.id === "gradeFilter") opt.textContent = `${v}. Klasse`;
           else if ($sel.id === "ageFilter") opt.textContent = ${v} Jahre;
           else if ($sel.id === "typeFilter") opt.textContent = labelType(v);
           else opt.textContent = v;
