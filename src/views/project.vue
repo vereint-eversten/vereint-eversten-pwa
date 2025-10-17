3400fae767ee9efcda9a0306020622bdb259e786
vereint-eversten <fab_f1ve@web.de>
2025-10-17 16:01:03 +0200

    Create project.vue

---
+++ src/views/project.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <h1>{{ t('project.title') }}</h1>
+    <p class="muted">Netzwerk wird hier integriert.</p>
+  </section>
+</template>
+<script setup>
+import { t } from '../lib/i18n'
+</script>
