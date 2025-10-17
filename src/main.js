eee43d4a53ed30134fd14587cab731aabce6ea39
vereint-eversten <fab_f1ve@web.de>
2025-10-17 15:02:51 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import { i18n } from './plugins/i18n'
+import './styles.css'
 
+const app = createApp(App)
+app.use(createPinia())
+app.use(i18n)
+app.use(router)
+app.mount('#app')
