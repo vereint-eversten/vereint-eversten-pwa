9f4fda3caa93948b01be5f8beb5a07a68b540c0e
vereint-eversten <fab_f1ve@web.de>
2025-10-17 15:00:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
+      manifest: {
+        name: 'Verein[t]',
+        short_name: 'Verein[t]',
+        theme_color: '#0b0b0b',
+        background_color: '#0b0b0b',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
+  server: { port: 5173 }
+})
