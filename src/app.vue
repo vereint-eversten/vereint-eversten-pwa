adbc24b13b726b7a7614851b09fcb5f675ef9cba
vereint-eversten <fab_f1ve@web.de>
2025-10-17 15:03:34 +0200

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,1 +1,16 @@
+<template>
+  <div class="app">
+    <TopBar />
 
+    <main class="page">
+      <router-view />
+    </main>
+
+    <BottomNav />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TopBar from './components/TopBar.vue'
+import BottomNav from './components/BottomNav.vue'
+</script>
