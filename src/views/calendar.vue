b88b08414a51293ffbaa12e2e7c233b94f929176
vereint-eversten <fab_f1ve@web.de>
2025-10-17 16:01:51 +0200

    Update calendar.vue

--- src/views/calendar.vue
+++ src/views/calendar.vue
@@ -1,1 +1,9 @@
-
+<template>
+  <section>
+    <h1>{{ t('calendar.title') }}</h1>
+    <p class="muted">Kalender-Module später einhängen.</p>
+  </section>
+</template>
+<script setup>
+import { t } from '../lib/i18n'
+</script>
