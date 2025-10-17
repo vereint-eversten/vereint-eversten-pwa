d4c096918603d76cf50809c83fcd945d1cfd8a37
vereint-eversten <fab_f1ve@web.de>
2025-10-17 16:02:47 +0200

    Update messenger.vue

--- src/views/messenger.vue
+++ src/views/messenger.vue
@@ -1,1 +1,9 @@
-
+<template>
+  <section>
+    <h1>{{ t('messenger.title') }}</h1>
+    <div class="card">Nur für eingeloggte Nutzer – später echte Chats.</div>
+  </section>
+</template>
+<script setup>
+import { t } from '../lib/i18n'
+</script>
